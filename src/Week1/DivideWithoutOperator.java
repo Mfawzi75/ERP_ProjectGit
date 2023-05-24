e76d2c22e6b3bfec50eb46f68df79c2954326cfa
sharada <gurungsharda3@gmail.com>
2023-05-24 13:36:58 -0400

    created method of DivideWithoutOperator

--- src/Week1/DivideWithoutOperator.java
+++ src/Week1/DivideWithoutOperator.java
@@ -1,4 +1,31 @@
 package Week1;
 
+import java.util.Scanner;
+
 public class DivideWithoutOperator {
-}
+
+            public static void main(String args[]){
+
+                int num1,num2,div,result = 0;
+
+                Scanner scan=new Scanner(System.in);
+
+                System.out.print("Enter the value to num1: ");
+                num1=scan.nextInt();
+
+                System.out.print("Enter the value to num2: ");
+                num2=scan.nextInt();
+                div=num1+num2;
+                while(div>num2){
+                    div=div-num2;
+                    result++;
+
+                }
+                System.out.println("Division of "+num1+" and "+num2+":"+result);
+            }
+        }
+
+
+
+
+

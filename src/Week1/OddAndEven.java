37a1a67a40f5e45499f42eeee7ccaead4e93476b
sharada <gurungsharda3@gmail.com>
2023-05-24 13:49:01 -0400

    created method of Odd&Even

--- src/Week1/OddAndEven.java
+++ src/Week1/OddAndEven.java
@@ -1,4 +1,21 @@
 package Week1;
 
 public class OddAndEven {
+    public static void main(String[] args) {
+
+        int number = 5;
+        int number2 = 6;
+
+        boolean isEven = number % 2 ==0;
+
+        boolean isOdd = !isEven;
+
+        System.out.println(number + " is an even number: " + isEven);
+        System.out.println(number2 + " is an odd number: " + isOdd);
+
+
+
+
+    }
 }
+

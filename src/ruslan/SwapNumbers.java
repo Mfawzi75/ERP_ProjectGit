3ad638c1057217941e898b35aee730da3b2e49d0
Ruslan_Amro <ruslan.amrayev85@gmail.com>
2023-05-26 20:23:03 -0400

    Renamed Task class and added SwapNumbers

---
+++ src/ruslan/SwapNumbers.java
@@ -0,0 +1,16 @@
+package src.ruslan;
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        int num1 = 25;
+        int num2 = 35;
+        System.out.println("Before swapping:" + "\nnum1 = " + num1 + "\nnum2 = " + num2);
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+        System.out.println("After swapping:" + "\nnum1 = " + num1 + "\nnum2 = " + num2);
+    }
+}
--- src/ruslan/Tasks.java
+++
@@ -1,4 +0,0 @@
-package src.ruslan;
-
-public class Tasks {
-}

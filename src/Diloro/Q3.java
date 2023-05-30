4de2606c7d22eea426ddbabf483ef8aef4abdb0a
dilosh12 <dilora753@gmail.com>
2023-05-29 20:49:01 -0400

    Finished Q3

---
+++ src/Diloro/Q3.java
@@ -0,0 +1,36 @@
+package Diloro;
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a positive number:");
+        int n = in.nextInt();
+
+        if (n > 0) {
+            for (int i = 1; i <= n; i++) {
+                if (i % 5 == 0 && i % 3 == 0 && i % 2 == 0) {
+                    System.out.println("CodilityTestCoders");
+                } else if (i % 3 == 0 && i % 2 == 0) {
+                    System.out.println("CodilityTest");
+                } else if (i % 5 == 0 && i % 3 == 0) {
+                    System.out.println("TestCoders");
+                } else if (i % 5 == 0 && i % 2 == 0) {
+                    System.out.println("CodilityCoders");
+                } else if (i % 5 == 0) {
+                    System.out.println("Coders");
+                } else if (i % 3 == 0) {
+                    System.out.println("Test");
+                } else if (i % 2 == 0) {
+                    System.out.println("Codility");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        }
+
+
+    }
+}

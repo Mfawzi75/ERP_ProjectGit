bf48ff08a31d6948835dc308194aa321b7e15c42
RuslanAmro <131870434+RuslanAmro@users.noreply.github.com>
2023-06-07 13:56:58 -0400

    Update ReverseNegative.java

--- src/ruslan/Week3/ReverseNegative.java
+++ src/ruslan/Week3/ReverseNegative.java
@@ -8,13 +8,21 @@ public class ReverseNegative {
 
     }
 
-    public static void reverseNegative(int num) {
+    public static void reverseNegative(int num) {// num: -215
 
         int reversed = 0;
 
         for (; num != 0; num /= 10) {
-            int digit = num % 10;
+            //1st iteration -> num: -215;  digit: -5
+            //2nd iteration -> num: -21;   digit: -1
+            //3st iteration -> num: -2;  digit: -2
+            
+            int digit = num % 10;//reminder of num --> now is (5)
+            
             reversed = reversed * 10 + digit;
+            //1st iteration -> reversed: 0;   digit: -5
+            //2nd iteration -> reversed: -5;  digit: -1
+            //3rd iteration -> reversed: -51;  digit: -2
         }
         System.out.println("Reversed number = " + reversed);
     }

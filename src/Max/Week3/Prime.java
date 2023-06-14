7674b835dd90796caa48cdf7284c9361a1c1a641
CydeoGit-Max <Thinkingmaximus@gmail.com>
2023-06-14 12:14:00 -0400

    Week4 New tasks

--- src/Max/Week3/Prime.java
+++ src/Max/Week3/Prime.java
@@ -1,18 +1,31 @@
 package Max.Week3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Prime {
     public static void main(String[] args) {
-        int num= 56;
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter the number");
+        int num=input.nextInt();
+        ArrayList<Integer> list=new ArrayList<>(num);
+        System.out.print ("Prime numbers: -");
 
-        for (int i = 1; i < num; i++) {
+        for (int i = 1; i < num+10; i++) {
            boolean count=false;
             for (int j = 2; j < i; j++) {
                 if(i%j==0){
                     count= true;
                 break;}
             }
-            if(!count)
-                System.out.print("  "+i);
+            if(!count){
+                list.add(i);
+            System.out.print("  "+i);}
+
         }
+        System.err.print("\n"+"Result:");
+        if(list.contains(num))
+            System.err.println(num +" - is prime number");
+        else System.err.println("\n"+num +" - is not prime number");
     }
 }
---
+++ src/Max/Week4/SameLaters.java
@@ -0,0 +1,29 @@
+package Max.Week4;
+
+import java.util.Scanner;
+
+public class SameLaters {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter first String");
+        String word1=input.nextLine();
+        System.out.println("Please enter Second String");
+        String word2=input.nextLine();
+
+        System.out.println("Result- "+method(word1,word2));
+
+    }
+
+    public static boolean method(String word1, String word2){
+        boolean result=true;
+        for (int i = 0; i < word1.length()-1; i++) {
+            if(!word2.contains(""+word1.charAt(i))){
+                 result=false;
+        break;}}
+        return result;
+
+
+    }
+}
+
+

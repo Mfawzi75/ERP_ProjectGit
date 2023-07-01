5e930b2d523b31d6b2bf6a0018d8dcd96432567b
Mfawzi75 <88123903+Mfawzi75@users.noreply.github.com>
2023-07-01 02:20:04 -0400

    added reverse method on week5

---
+++ src/Mo/Week5/FindUnique.java
@@ -0,0 +1,9 @@
+package Mo.Week5;
+
+public class FindUnique {
+    /*
+    Find the unique:
+  Write a return method that can find the unique characters from the String.
+  Ex: unique("AAABBBCCCDEF") ==> "DEF";
+     */
+}
---
+++ src/Mo/Week5/Reverse.java
@@ -0,0 +1,9 @@
+package Mo.Week5;
+
+public class Reverse {
+    /*
+    Reverse:
+  Write a return method that can reverse String.
+  Ex: Reverse("ABCD"); ==> DCBA
+     */
+}

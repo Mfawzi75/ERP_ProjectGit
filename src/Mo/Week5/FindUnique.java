2270c251da9bb21417c618d9fe0e6e785785dadc
Mfawzi75 <88123903+Mfawzi75@users.noreply.github.com>
2023-07-01 09:12:56 -0400

    added reverse method on week5

--- src/Mo/Week5/FindUnique.java
+++ src/Mo/Week5/FindUnique.java
@@ -1,9 +1,41 @@
 package Mo.Week5;
 
+import java.util.Scanner;
+
 public class FindUnique {
     /*
     Find the unique:
   Write a return method that can find the unique characters from the String.
   Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
      */
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("enter String");
+        String str = input.nextLine();
+        System.out.println("result = "+unique(str));
+
+    }
+
+    public static String unique(String str){
+        boolean flag = true;
+        String result="";
+        for (int i = 0; i <= str.length()-1; i++) {
+            flag = true;
+            for (int j = 0; j < str.length(); j++) {
+                if(i==j)
+                    continue;
+                if (str.charAt(i) == str.charAt(j)){
+                    flag=false;
+                    break;}
+
+            }
+            if (flag)
+                result += str.charAt(i);
+        }
+
+
+        return result;
+    }
+
 }
